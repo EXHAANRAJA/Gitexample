6545028624ff40f9f9d3b47e431e399e436fc7ff
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-26 14:24:28 +0500

    Color & Size added in scatter plot

--- Python/Matplotlib/Scatter Plot.py
+++ Python/Matplotlib/Scatter Plot.py
@@ -1,10 +1,13 @@
+from matplotlib import colors
 import matplotlib.pyplot as plt
+
 
 year = [2010, 2012, 2014, 2016, 2018, 2020, 2021]
 pop = [179.4, 187.3, 195.3, 203.6, 204.73, 208.57, 212.48]
 
 # Build a Line Chart
-plt.scatter(year, pop)
+plt.scatter(x=year, y=pop, color='green', s=10)
+plt.grid()
 
 # Setting Label in x & y cordinate
 plt.xlabel("Year Wise Data")

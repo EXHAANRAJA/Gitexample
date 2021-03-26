e039c4f568aee2073bbae7abd8b4d632d00ce41f
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-26 20:04:39 +0500

    Pie Chart Added

---
+++ Python/Matplotlib/Pie Chart.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('bmh')
+
+province = ['punjab','sindh','kpk','Balochistan','Islamabad']
+population = [52,18,20,10,26]
+explode = [0,0.025,0,0,0]
+
+color=['#118DFF','#99E4AD','#FB87A2','brown','green']
+
+plt.pie(population,labels=province ,shadow=True
+    ,explode=explode, colors=color,autopct="%1.1f%%", wedgeprops={'edgecolor':'black'})
+
+plt.title("PAKISTAN ADMINISTRATIVE UNIT")
+plt.show()

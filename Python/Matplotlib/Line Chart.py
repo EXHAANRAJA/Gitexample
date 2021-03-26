5d6e150eb699a262b2b9ab53040b8ca2e89fd590
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-26 12:55:02 +0500

    First Commit

---
+++ Python/Matplotlib/Line Chart.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+year = [2010, 2012, 2014, 2016, 2018, 2020, 2021]
+pop = [179.4, 187.3, 195.3, 203.6, 204.73, 208.57, 212.48]
+
+# Build a Line Chart
+plt.plot(year, pop)
+
+# Setting Label in x & y cordinate
+plt.xlabel("Year Wise Data")
+plt.ylabel("Population in Million")
+plt.title("Pakistan Population Projection")
+
+plt.show()

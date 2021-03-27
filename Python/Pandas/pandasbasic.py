9e328bdb38d152564db85175bb2eb7233372367f
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-27 16:10:17 +0500

    Row Label Added by using indexing

--- Python/Pandas/pandasbasic.py
+++ Python/Pandas/pandasbasic.py
@@ -5,7 +5,10 @@ dr = [True, False, False, False, True, True, True]
 cpc = [809, 731, 588, 18, 200, 70, 45]
 
 my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
-
 cars = pd.DataFrame(my_dict)
+
+# Set the row label ,Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+cars.index = row_labels
 
 print(cars)

c15252e215f48cced1b24d40b874508b14c3bee4
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-28 12:58:31 +0500

    Build in function orientation

--- Python/Pandas/pandas dataframe.py
+++ Python/Pandas/pandas dataframe.py
@@ -12,3 +12,24 @@ row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
 cars.index = row_labels
 
 print(cars)
+
+# We can used number of scalar function inside pandas dataframe
+
+cars['cars_per_cap'].max  # Display max number inside list
+cars['cars_per_cap'].min  # Display min number inside list
+
+# or by using series
+print(cars.max())
+print(cars.min())
+
+# to display statistical component
+print(cars.describe())
+
+# During Pandas Dataframe first 5 and last 5 rows display by default
+cars.head(8)  # Display first 8 rows
+print(cars.dtypes) # Display column datatype
+print(cars.info())  # Print Column name,Not Null Statistics and Datatype
+print(type(cars))  # Display dataframe
+print(cars['country']) # each individual column display a series
+
+print(cars['cars_per_cap'] < 500)

2f5856bb5be4e7f2ee4b3a00b231d54a8fbfcb1d
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-04-12 11:00:49 +0500

    List Tutorial From Now

---
+++ Python/list/listcounter.py
@@ -0,0 +1,5 @@
+# Count of List Key by using Counter
+gridstaff = ['Musaddiq','Tahir','Anayat','Rizwan','Adeel','Roman','Habib','Tahir']
+from collections import Counter
+totalstaff = Counter(gridstaff)
+print(totalstaff)
